add9ec16be7adc43b28cfc053e9c995b932cd3e1
Rex <rex.xie@qq.com>
2017-03-02 09:34:08 +0800

    more error information.

---
+++ APDilution/InputBarcodes/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace InputBarcodes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBatchCnt = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(161, 425);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 0;
+            this.btnSet.Text = "设置";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 430);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "批次数：";
+            // 
+            // txtBatchCnt
+            // 
+            this.txtBatchCnt.Location = new System.Drawing.Point(69, 426);
+            this.txtBatchCnt.Name = "txtBatchCnt";
+            this.txtBatchCnt.Size = new System.Drawing.Size(86, 20);
+            this.txtBatchCnt.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(17, 25);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(308, 286);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "条码：";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Enabled = false;
+            this.btnOk.Location = new System.Drawing.Point(246, 425);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "确定";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(17, 341);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(308, 69);
+            this.txtInfo.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "提示：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 458);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.txtBatchCnt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSet);
+            this.Name = "Form1";
+            this.Text = "条码设置";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBatchCnt;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ APDilution/InputBarcodes/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InputBarcodes
+{
+    public partial class Form1 : Form
+    {
+        int batchCnt = 0;
+        List<string> barcodes = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            FolderHelper.WriteResult(false);
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            string sBatchCnt = txtBatchCnt.Text;
+            if(sBatchCnt == "")
+            {
+                SetErrorInfo("请设置批次数！");
+                return;
+            }
+            
+            bool bInt = int.TryParse(sBatchCnt, out batchCnt);
+            if (!bInt)
+            {
+                SetErrorInfo("批次数必须为数字！");
+                return;
+            }
+            if(batchCnt <1 || batchCnt > 16)
+            {
+                SetErrorInfo("批次数必须在1~16之间！");
+                return;
+            }
+            btnOk.Enabled = true;
+            InitDatagridView();
+        }
+
+        private void InitDatagridView()
+        {
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.EnableHeadersVisualStyles = false;
+            dataGridView.Columns.Clear();
+
+            //for (int i = 0; i < 2; i++)
+            {
+                DataGridViewTextBoxColumn column = new DataGridViewTextBoxColumn();
+                column.HeaderText = "反应板条码";
+                column.HeaderCell.Style.BackColor = Color.LightSeaGreen;
+                dataGridView.Columns.Add(column);
+                dataGridView.Columns[0].SortMode = DataGridViewColumnSortMode.Programmatic;
+            }
+            dataGridView.RowHeadersWidth = 80;
+            dataGridView.Rows.Add(batchCnt);
+            for (int i = 0; i < batchCnt; i++)
+            {
+                dataGridView.Rows[i].HeaderCell.Value = string.Format("批次{0}", i + 1);
+            }
+        }
+
+
+        private void SetErrorInfo(string info)
+        {
+            txtInfo.Text = info;
+            txtInfo.ForeColor = Color.Red;
+            txtInfo.BackColor = Color.White;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckBarcodes();
+                Save2File();
+            } 
+            catch(Exception ex)
+            {
+                SetErrorInfo(ex.Message);
+                return;
+            }
+            FolderHelper.WriteResult(true);
+            this.Close();
+        }
+
+        private void Save2File()
+        {
+            string sFolder = FolderHelper.GetOutputFolder();
+            if (Directory.Exists(sFolder))
+                Directory.CreateDirectory(sFolder);
+            string sFile = sFolder + "barcodes.txt";
+            File.WriteAllLines(sFile, barcodes);
+
+        }
+
+        private void CheckBarcodes()
+        {
+            barcodes.Clear();
+            
+            for (int i = 0; i < batchCnt; i++)
+            {
+                for (int indexInBatch = 0; indexInBatch < 1; indexInBatch++)
+                {
+                    if(dataGridView.Rows[i].Cells[indexInBatch].Value == null)
+                        throw new Exception(string.Format("批次：{0}的第{1}个条码为空！", i + 1, 1+indexInBatch));
+                    var tmpStr = dataGridView.Rows[i].Cells[indexInBatch].Value.ToString();
+                    if(tmpStr == "")
+                        throw new Exception(string.Format("批次：{0}的第{1}个条码为空！",i+1,1+indexInBatch));
+                    
+                    if (barcodes.Contains(tmpStr))//already exist
+                    {
+                        int index = barcodes.IndexOf(tmpStr);
+                        throw new Exception(string.Format("批次{0}和批次{1}的条码重复！", i+1, index + 1));
+                    }
+                    barcodes.Add(tmpStr);
+                }
+            }
+        }
+    }
+}
